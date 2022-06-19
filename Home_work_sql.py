53a352a4d72f20a2ecce60eca776484d8276fcba
Lopatina Darya <baboleh@rambler.ru>
2022-06-20 01:29:29 +0300

    - Создали базу данных MusicSite.db - Создали таблицы Genre, Musician, Album, Track в базе данных MusicSite.db

---
+++ Home_work_sql.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+with sqlite3.connect("MusicSite.db") as db:
+    cursor = db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Genre(
+id integer PRIMARY KEY,
+name text NOT NULL);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Musician(
+id integer PRIMARY KEY,
+name text NOT NULL,
+genre_id integer REFERENCES Genre(id));""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Album(
+id integer PRIMARY KEY,
+musician_id integer REFERENCES Musician(id),
+name text NOT NULL,
+year integer NOT NULL);""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Track(
+id integer PRIMARY KEY,
+album_id integer REFERENCES Album(id),
+name text NOT NULL,
+duration integer NOT NULL);""")
+
+db.close()

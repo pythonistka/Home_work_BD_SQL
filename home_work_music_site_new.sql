7c8f10bdbdc6eab0932f32ea402e3e25d9d4aef9
Lopatina Darya <baboleh@rambler.ru>
2022-06-25 13:25:13 +0300

    Текстовый файл с командами SQL для создания таблиц в базе данных MusicSiteNewVersion.db

---
+++ home_work_music_site_new.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS Genre(
+id integer PRIMARY KEY,
+name text UNIQUE NOT NULL)
+
+CREATE TABLE IF NOT EXISTS Musician(
+id integer PRIMARY KEY,
+name text UNIQUE NOT NULL)
+
+CREATE TABLE IF NOT EXISTS MusicianGenre(
+genre_id integer REFERENCES Genre(id),
+musician_id integer REFERENCES Musician(id),
+constraint pk PRIMARY KEY (genre_id, musician_id))
+
+CREATE TABLE IF NOT EXISTS Album(
+id integer PRIMARY KEY,
+name text NOT NULL,
+year integer NOT NULL)
+
+CREATE TABLE IF NOT EXISTS MusicianAlbum(
+musician_id integer REFERENCES Musician(id),
+album_id integer REFERENCES Album(id),
+constraint pk PRIMARY KEY (musician_id, album_id))
+
+CREATE TABLE IF NOT EXISTS Track(
+id integer PRIMARY KEY,
+album_id integer REFERENCES Album(id) NOT NULL,
+name text NOT NULL,
+duration time NOT NULL)
+
+CREATE TABLE IF NOT EXISTS Collection(
+id integer PRIMARY KEY,
+name text NOT NULL,
+year integer NOT NULL)
+
+CREATE TABLE IF NOT EXISTS CollectionTrack(
+track_id integer REFERENCES Track(id),
+collection_id integer REFERENCES Collection(id),
+constraint pk PRIMARY KEY (track_id, collection_id))

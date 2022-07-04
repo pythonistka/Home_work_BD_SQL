bcefaae18528ea1e73f0c81f96ce378a28e9e079
Lopatina Darya <baboleh@rambler.ru>
2022-07-04 23:39:45 +0300

    Текстовый файл с командами Select SQL для запросов из таблиц в базе данных

---
+++ select_home_work.sql
@@ -0,0 +1,17 @@
+SELECT Album.name, Album.year
+FROM Album WHERE year = 2018
+
+SELECT Track.name, Track.duration
+FROM Track WHERE duration = (SELECT MAX (duration) FROM Track)
+
+SELECT Track.name
+FROM Track WHERE duration > "3:5"
+
+SELECT Collection.name
+FROM Collection WHERE 2018 < year < 2020
+
+SELECT Musician.name
+FROM Musician WHERE name NOT LIKE '% %'
+
+SELECT Track.name
+FROM Track WHERE name LIKE '%%мой%%'

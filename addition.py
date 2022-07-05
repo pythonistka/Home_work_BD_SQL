d5ff7c3a02873e735b140fa4fce0baa729db1aef
Lopatina Darya <baboleh@rambler.ru>
2022-07-06 02:09:11 +0300

    Добавлены данные в базу MusicSiteNewVersion.db

--- addition.py
+++ addition.py
@@ -3,16 +3,21 @@ import sqlite3
 with sqlite3.connect("MusicSiteNewVersion.db") as db:
     cursor = db.cursor()
 
+cursor.execute("""INSERT INTO Album(name, year)
+VALUES("Cat Style", "2020")""")
+db.commit()
+
 cursor.execute("""INSERT INTO MusicianAlbum(musician_id, album_id)
-VALUES("4", "9")""")
+VALUES("2", "10")""")
 db.commit()
 
 cursor.execute("""INSERT INTO Track(album_id, name, duration)
-VALUES("9", "Гучи", "3:31")""")
+VALUES("10", "Cat Cat Cat", "3:15")""")
 db.commit()
 
 cursor.execute("""INSERT INTO CollectionTrack(track_id, collection_id)
-VALUES("17", "8")""")
+VALUES("18", "4")""")
 db.commit()
 
 db.close()
+

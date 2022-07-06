d95ca94e809fab9897f7e225ecad74d16ec8c3b5
Lopatina Darya <baboleh@rambler.ru>
2022-07-06 22:54:20 +0300

    Создана база MusicSiteNewCorrectVersion.db

--- Home_work_sql2.py
+++ Home_work_sql2.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-with sqlite3.connect("MusicSiteNewVersion.db") as db:
+with sqlite3.connect("MusicSiteNewCorrectVersion.db") as db:
     cursor = db.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS Genre(
@@ -9,22 +9,24 @@ name text UNIQUE NOT NULL);""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS Musician(
 id integer PRIMARY KEY,
-name text UNIQUE NOT NULL);""")
+name text UNIQUE NOT NULL,
+genre_id integer REFERENCES Genre(id) NOT NULL);""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS MusicianGenre(
 genre_id integer REFERENCES Genre(id),
 musician_id integer REFERENCES Musician(id),
-constraint pk PRIMARY KEY (genre_id, musician_id));""")
+constraint MusicianGenre_genre_id_musician_id_pk PRIMARY KEY (genre_id, musician_id));""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS Album(
 id integer PRIMARY KEY,
 name text NOT NULL,
-year integer NOT NULL);""")
+year integer NOT NULL,
+musician_id integer REFERENCES Musician(id) NOT NULL);""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS MusicianAlbum(
 musician_id integer REFERENCES Musician(id),
 album_id integer REFERENCES Album(id),
-constraint pk PRIMARY KEY (musician_id, album_id));""")
+constraint MusicianAlbum_musician_id_album_id_pk PRIMARY KEY (musician_id, album_id));""")
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS Track(
 id integer PRIMARY KEY,
@@ -40,6 +42,6 @@ year integer NOT NULL);""")
 cursor.execute("""CREATE TABLE IF NOT EXISTS CollectionTrack(
 track_id integer REFERENCES Track(id),
 collection_id integer REFERENCES Collection(id),
-constraint pk PRIMARY KEY (track_id, collection_id));""")
+constraint CollectionTrack_track_id_collection_id_pk PRIMARY KEY (track_id, collection_id));""")
 
 db.close()
